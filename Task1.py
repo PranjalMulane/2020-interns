83465c6ba0b4d8878d7f9e10558883fd30269a71
PranjalMulane <52007480+PranjalMulane@users.noreply.github.com>
2020-07-22 00:38:11 +0530

    Task -2
    Task -2

--- Task1.py
+++ Task1.py
@@ -1,5 +1,6 @@
 
-
+#WINSOFT INTERNSHIP TASK
+##########################################################################3
 import json 
 import collections
 import pandas as pd
@@ -20,10 +21,11 @@ val = list(rates.values())
 
 df = pd.DataFrame(val, index = datelist)
 
+#-------------------TASK-1---------------------------
+
 inr = (df.loc[:'INR'])
 
 inrval = (list(inr))
-
 
 plt.plot(datelist[:30], inrval[:30])    
 
@@ -31,13 +33,25 @@ plt.xlabel('Dates')
 
 plt.ylabel('EUR to INR')
 
+plt.title('Graph of INR exchange rate against EUR') 
+
 plt.show()
 
+#--------------------TASK - 2------------------------
 
+inr = (df.loc[:'INR','GBP'])
 
+inrval = (list(inr))
 
+plt.plot(datelist[:30], inrval[:30])    
 
+plt.xlabel('Dates')
 
+plt.ylabel('EUR to INR and GBP')
+
+plt.title('Graph of INR and GBP exchange rate against EUR') 
+
+plt.show()
 
 
 
