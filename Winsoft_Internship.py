8917ea68ae090dc3c786cceb9880ca6d9343ef44
PranjalMulane <52007480+PranjalMulane@users.noreply.github.com>
2020-07-22 10:31:44 +0530

    Task 3
    Task 3

--- Winsoft_Internship.py
+++ Winsoft_Internship.py
@@ -54,6 +54,46 @@ plt.title('Graph of INR and GBP exchange rate against EUR')
 plt.show()
 
 
+#--------------------TASK 3----------------------------
+
+
+newDataFile = open('latest-rates.json')
+
+newData = json.load(newDataFile)
+
+newRates = (data['rates'])
+
+newOd = collections.OrderedDict(sorted(newRates.items()))
+
+newDatelist = list(newOd.keys())
+
+newVal = list(newRates.values())
+
+newDf = pd.DataFrame(newVal, index = newDatelist)
+
+newInr = (newDf.loc[:'INR','GBP'])
+
+newInrval = (list(newInr))
+
+
+
+inr = (df.loc[:'INR','GBP'])
+inrval = (list(inr))
+
+plt.plot(newDatelist[:30], newInrval[:30], label = 'latest rates') 
+
+plt.plot(datelist[:30], inrval[:30], label = 'Jan 2019')   
+
+plt.xlabel('Dates')
+
+plt.ylabel('EUR to INR and GBP')
+
+plt.title('Graph of INR and GBP exchange rate against EUR') 
+
+plt.show()
+
+
+
 
 
 
